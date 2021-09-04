5dc4c46973908726fe4c555f989bf111ed14401f
Shim239 <shim.goodevil@yandex.ru>
2021-09-04 09:42:44 +0300

    Commit message

---
+++ src/Helloworld.java
@@ -0,0 +1,13 @@
+/*
+
+first java program
+by SmirnovSM on 04.09.2021
+
+ */
+
+
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
