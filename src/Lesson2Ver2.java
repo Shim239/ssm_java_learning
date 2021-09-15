8a736ead4fa83372075f61d043b407c193cb7bf9
Shim239 <shim.goodevil@yandex.ru>
2021-09-15 16:35:00 +0300

    lesson 2 version 2

--- src/Lesson2Ver2.java
+++ src/Lesson2Ver2.java
@@ -4,12 +4,12 @@ public class Lesson2Ver2 {
     public static void main(String[] args) {
         String[][] clients = new String[][] {
                 {"25", "Ivan", "male"},
-                {"20", "Lilia", "female"},
+                {"16", "Lilia", "female"},
                 {"33", "Felicia", "female"},
                 {"18", "Andrew", "male"},
-                {"18", "Isabelle", "female"},
+                {"34", "Isabelle", "female"},
                 {"62", "Agatha", "female"},
-                {"57", "Crispin", "female"},
+                {"47", "Crispin", "male"},
                 {"53", "Piers", "male"},
                 {"48", "Nora", "female"},
                 {"29", "Ada", "female"}
@@ -25,13 +25,15 @@ public class Lesson2Ver2 {
         // переводим массив возрастов в int для возможности сравнения <, >
         int[] intAge = Arrays.asList(age).stream().mapToInt(Integer::parseInt).toArray(); // это нагугленая строчка
 
+
         // объявляем переменные для сравнения элементов массива
         int tempAge;
+        String[] tempGender = new String[clients.length];
         String[] tempClients = new String[clients.length];
 
         // ставим элементы массива по возрастанию
-        for (int i = 0; i < intAge.length; i++) {
-            for (int j = i+1; j < intAge.length; j++) {
+        for (int i = 0; i < clients.length; i++) {
+            for (int j = i+1; j < clients.length; j++) {
                 if (intAge[i] > intAge[j]) {
                     tempAge = intAge[i]; // здесь работаем с int массивом возрастов
                     intAge[i] = intAge[j];
@@ -41,15 +43,20 @@ public class Lesson2Ver2 {
                     clients[i][1] = clients[j][1];
                     clients[j][1] = tempClients[i];
 
+                    tempGender[i] = clients[i][2]; // здесь меняем местами данные в строках двумерного массива
+                    clients[i][2] = clients[j][2];
+                    clients[j][2] = tempGender[i];
+
                 }
             }
         }
 
+        System.out.println();
         System.out.println("Выводим только женщин с сортировкой по возрасту!\n");
 
         for (int i = 0; i < clients.length; i++) {
             if (clients[i][2] == "female") {
-                System.out.println(clients[i][1] + ", age: " + intAge[i] + ", " + clients[i][2]);
+                System.out.println(clients[i][1] + ", age: " + intAge[i]);
             }
 
 
