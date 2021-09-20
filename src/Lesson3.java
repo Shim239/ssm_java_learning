bb91938f8d40234a72be280675ce00231062353a
Shim239 <shim.goodevil@yandex.ru>
2021-09-20 12:45:45 +0300

    lesson 3 version 1

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -1,10 +1,10 @@
 /*
 
 В коде зашиты две строки.
-Нужно проверить (вывести true или false), являются ли две эти строки анаграммами (регистр и пробелы не важны)
+Нужно проверить, являются ли две эти строки анаграммами (регистр и пробелы не важны)
 
 by SmirnovSM
-17.09.2021
+20.09.2021
 
 ver 1.0
 
@@ -14,42 +14,39 @@ import java.util.Locale;
 
 public class Lesson3 {
     public static void main(String[] args) {
-        String string1 = "Я в мире - сирота";
-        String string2 = "Я в Риме - Ариост";
-        String string3 = "";
+        String string1 = "ав стр ал  оП итек ";
+        String string2 = "вАте  рпол  ис  тКа";
+        int sum1 = 0;
+        int sum2 = 0;
 
         // приводим строки к нижнему регистру и убираем все пробелы
         string1 = string1.toLowerCase(Locale.ROOT).replaceAll("\\s", "");
         string2 = string2.toLowerCase(Locale.ROOT).replaceAll("\\s", "");
 
-        //сравниваем длины измененных строк (может вообще дальше не нужно идти)
+        // сравниваем длины измененных строк (может вообще дальше не нужно идти)
         if (string1.length() == string2.length()) {
+            char[] charString1 = string1.toCharArray();
+            char[] charString2 = string2.toCharArray();
 
-            //побуквенно перебираем строку1
-            for (int i = 0; i < string1.length(); i++) {
-                String letter = string1.substring(i, i + 1);
-
-                // если в строке 2 присутствует буква из строки 1, то добавляем ее в строку 3
-                    if (string2.indexOf(letter) != -1) {
-                        string3 += letter;
-                }
+            // у каждого char символа есть свой код (порядковый номер), если сумма кодов равна - то значит строки есть анаграммы
+            for (int i = 0; i < charString1.length; i++) {
+                sum1 += charString1[i];
+                sum2 += charString2[i];
 
             }
 
-        }
+            // проверяем контрольные суммы по двум строкам
+            if (sum1 == sum2) {
+                System.out.println("Буквосочетания являются анаграммами");
+            }
+            else {
+                System.out.println("Буквосочетания не являются анаграммами");
+            }
 
-        // если у строк разная длина
+         }
+
         else {
             System.out.println("У строк (после удаления пробелов) разная длина, они явно не анаграммы!");
         }
-        /* вывод строк 1 2 и 3
-        System.out.println("1 = " + string1);
-        System.out.println("2 = " + string2);
-        System.out.println("3 = " + string3);
-        */
-
-        // вывод в консоль результата сравнения строк 1 и 3
-        System.out.println(string1.equals(string3));
-
     }
 }
