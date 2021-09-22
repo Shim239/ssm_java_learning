7fdf393c8b095f214a2b4d236246c5e389e857a2
Shim239 <shim.goodevil@yandex.ru>
2021-09-22 11:54:55 +0300

    lesson 3

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -10,43 +10,52 @@ ver 1.0
 
  */
 
+import java.util.Arrays;
 import java.util.Locale;
 
 public class Lesson3 {
     public static void main(String[] args) {
-        String string1 = "ав стр ал  оП итек ";
-        String string2 = "вАте  рпол  ис  тКа";
-        int sum1 = 0;
-        int sum2 = 0;
+        String string1 = "ват  ерпо лИст ка";
+        String string2 = "а вСт  рал опитек";
 
         // приводим строки к нижнему регистру и убираем все пробелы
         string1 = string1.toLowerCase(Locale.ROOT).replaceAll("\\s", "");
         string2 = string2.toLowerCase(Locale.ROOT).replaceAll("\\s", "");
 
         // сравниваем длины измененных строк (может вообще дальше не нужно идти)
-        if (string1.length() == string2.length()) {
+        if (string1.length() != string2.length()) {
+            System.out.println("У строк (после удаления пробелов) разная длина, они явно не анаграммы!");
+            }
+        else {
+            // объявляем char массивы для строк
             char[] charString1 = string1.toCharArray();
             char[] charString2 = string2.toCharArray();
 
-            // у каждого char символа есть свой код (порядковый номер), если сумма кодов равна - то значит строки есть анаграммы
+            // сортируем char массивы по очереди
             for (int i = 0; i < charString1.length; i++) {
-                sum1 += charString1[i];
-                sum2 += charString2[i];
+                for (int j = i + 1; j < charString1.length; j++) {
+                    if (charString1[i] < charString1[j]) {
+                        char temp = charString1[i];
+                        charString1[i] = charString1[j];
+                        charString1[j] = temp;
+                    }
 
+                }
             }
+            for (int i = 0; i < charString2.length; i++) {
+                for (int j = i + 1; j < charString2.length; j++) {
+                    if (charString2[i] < charString2[j]) {
+                        char temp = charString2[i];
+                        charString2[i] = charString2[j];
+                        charString2[j] = temp;
+                    }
 
-            // проверяем контрольные суммы по двум строкам
-            if (sum1 == sum2) {
-                System.out.println("Буквосочетания являются анаграммами");
+                }
             }
-            else {
-                System.out.println("Буквосочетания не являются анаграммами");
-            }
+            // сравниваем получившиеся массивы (тут запись строки подсказала idea)
+            System.out.println(Arrays.equals(charString2, charString1));
 
-         }
-
-        else {
-            System.out.println("У строк (после удаления пробелов) разная длина, они явно не анаграммы!");
         }
+
     }
 }
