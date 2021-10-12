e70bc40d766ba37dca1b9e83b94eeb50696729a5
Shim239 <shim.goodevil@yandex.ru>
2021-10-12 14:57:46 +0300

    Lesson 5

--- src/Lesson5/CustomerGenerator.java
+++ src/Lesson5/CustomerGenerator.java
@@ -1,10 +1,12 @@
+/*
+Интерфейс генерации клиентов, содержащий в себе
+метод generateCustomer класса Customer для генерации клиентов
+
+интерфейс реализуется двумя классами - IndividualGenerator, EntityGenerator
+ */
+
 package Lesson5;
 
 public interface CustomerGenerator {
     Customer generateCustomer();
 }
-
-// Интерфейс генерации клиентов, содержащий в себе
-// метод generateCustomer класса Customer для генерации клиентов
-
-// интерфейс реализуется двумя классами - IndividualGenerator, EntityGenerator
