ea9768acd75870c34d712cac636c3c03166ac54b
Shim239 <shim.goodevil@yandex.ru>
2021-12-02 09:54:13 +0300

    Lesson 11

--- src/Lesson11/Main.java
+++ src/Lesson11/Main.java
@@ -31,64 +31,68 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
+    public static Object mutex = new Object();
+
     public static void main(String[] args) throws InterruptedException {
         SearchFiles searchFiles = new SearchFiles();
         System.out.println("Number of LOG files found: " + searchFiles.files.size() + '\n');
 
         for (File file : searchFiles.files) {
             Runnable task = () -> {
+                synchronized (mutex) {
 
-                ArrayList<LogObject> logObjects = new ArrayList<>();
-                System.out.println(Thread.currentThread().getName() + " add " + file); // выводим наименование файла, из которого выводятся логи
+                    ArrayList<LogObject> logObjects = new ArrayList<>();
+                    System.out.println(Thread.currentThread().getName() + " add " + file); // выводим наименование файла, из которого выводятся логи
 
-                // добавляем строки файлов log в список lines
-                List<String> lines = null;
-                try {
-                    lines = org.apache.commons.io.FileUtils.readLines(new File(String.valueOf(file)));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                // парсинг файла
-                for (String line : lines) {
-                    String timestamp = line.substring(0, 23);
-
-                    int levelLastIndex = line.indexOf(' ', 26);
-                    String level = line.substring(26, levelLastIndex);
-
-                    int loggerFirstIndex = line.indexOf('[', levelLastIndex);
-                    int loggerLastIndex = line.lastIndexOf(']');
-                    String logger = line.substring(loggerFirstIndex, loggerLastIndex + 1);
-
-                    int messageFirstIndex = line.lastIndexOf('-');
-                    int messageLastIndex = line.length();
-                    String message = line.substring(messageFirstIndex + 2, messageLastIndex);
-
-                    int sourceFirstIndex = file.toString().lastIndexOf('\\');
-                    String source = file.toString().substring(sourceFirstIndex + 1);
-
-                    if (level.equals("WARN")) {
-                        logObjects.add(new LogObject(timestamp, level, logger, message, source));
-
+                    // добавляем строки файлов log в список lines
+                    List<String> lines = null;
+                    try {
+                        lines = org.apache.commons.io.FileUtils.readLines(new File(String.valueOf(file)));
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
+
+                    // парсинг файла
+                    for (String line : lines) {
+                        String timestamp = line.substring(0, 23);
+
+                        int levelLastIndex = line.indexOf(' ', 26);
+                        String level = line.substring(26, levelLastIndex);
+
+                        int loggerFirstIndex = line.indexOf('[', levelLastIndex);
+                        int loggerLastIndex = line.lastIndexOf(']');
+                        String logger = line.substring(loggerFirstIndex, loggerLastIndex + 1);
+
+                        int messageFirstIndex = line.lastIndexOf('-');
+                        int messageLastIndex = line.length();
+                        String message = line.substring(messageFirstIndex + 2, messageLastIndex);
+
+                        int sourceFirstIndex = file.toString().lastIndexOf('\\');
+                        String source = file.toString().substring(sourceFirstIndex + 1);
+
+                        if (level.equals("WARN")) {
+                            logObjects.add(new LogObject(timestamp, level, logger, message, source));
+
+                        }
+                    }
+
+                    // делаем map, где ключ - message, а value - вся строка
+                    Map<String, LogObject> map = logObjects.stream().collect(Collectors.toMap(
+                            LogObject::getMessage,
+                            Function.identity(),
+                            (logObjects1, logObjects2) -> logObjects1));
+                    map.values().forEach(System.out::println);
+
+                    // пустая строка между файлами для вывода
+                    System.out.println();
+
                 }
-
-                // делаем map, где ключ - message, а value - вся строка
-                Map<String, LogObject> map = logObjects.stream().collect(Collectors.toMap(
-                        LogObject::getMessage,
-                        Function.identity(),
-                        (logObjects1, logObjects2) -> logObjects1));
-                map.values().forEach(System.out::println);
-
-                // пустая строка между файлами для вывода
-                System.out.println();
-
             };
 
             //for (int i = 0; i < searchFiles.files.size(); i++) {
                 Thread thread = new Thread(task);
                 thread.start();
-                thread.join();
+            //thread.join();
             // }
 
         }
